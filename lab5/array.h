360d4075113a962781f71974e15ecdbc2397718a
posteronee <95945156+posteronee@users.noreply.github.com>
2022-05-07 17:33:39 +0300

    Add files via upload

---
+++ lab5/array.h
@@ -0,0 +1,75 @@
+#ifndef LAB5_ARRAY_H
+#define LAB5_ARRAY_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class BasException{
+protected:
+    string error;
+public:
+    virtual string check() = 0;
+};
+
+class NonExistentIndex: public BasException{
+public:
+    NonExistentIndex(){
+        error = "No such index in this array";
+    }
+
+    string check() override{
+        return error;
+    }
+};
+
+class CheckSize: public BasException{
+public:
+    CheckSize(){
+        error = "Please check your size again";
+
+    }
+};
+
+template<int N, typename T>
+class Array{
+private:
+    vector<T> v1;
+
+    T IndexReference(T index){
+        if (index < 0 || index > N){
+            throw NonExistentIndex();
+        }
+        cout << v1[index];
+    }
+
+public:
+    Array(){
+        v1.resize(N);
+    }
+
+    void fillArray(){
+        for (int i = 1; i <= N; i++){
+            cin >> v1[i];
+        }
+    }
+
+    void showArray(){
+        for (int i = 1; i <= N; i++){
+            cout << v1[i] << " ";
+        }
+    }
+
+    void tryIndex(T index){
+        try{
+            IndexReference(index);
+        }catch (NonExistentIndex error){
+            cout << error.check() << endl;
+        }
+    }
+
+};
+
+
+
+
+#endif //LAB5_ARRAY_H
